7724c84c929f05c8550ef8ab5f56cd7652970f7c
raullima2 <raulfotografialima@gmail.com>
2022-10-23 23:30:47 -0300

    fix: fixação do problema do grid da calculadora

--- maven/src/main/java/br/com/calculadora_java/mvc/maven/CalculadoraView.java
+++ maven/src/main/java/br/com/calculadora_java/mvc/maven/CalculadoraView.java
@@ -19,10 +19,12 @@ public class CalculadoraView extends Application {
         public void start(Stage stage) throws IOException {
             String[] btnStringNumbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
             String[] btnStringOperators = {"+", "-", "*", "/"};
-            String[] btnStringOthers = {"CE", "=", ".", "DEL"};
+            String[] btnStringMedia = {"Mc", "Mr", "M+", "M-", "MS"};
+            String[] btnStringOthers = {"CE", "=", ".", "C"};
             Button[] btnNumbers = new Button[10];
-            Button[] btnOperators = new Button[5];
-            Button[] btnOthers = new Button[5];
+            Button[] btnOperators = new Button[4];
+            Button[] btnMedia = new Button[5];
+            Button[] btnOthers = new Button[4];
 
             GridPane gridPane = new GridPane();
             
@@ -30,15 +32,14 @@ public class CalculadoraView extends Application {
             CalculadoraController.textField.setFont(javafx.scene.text.Font.font("System", 20));
             //Creating a buttons for numbers and Operators
             createGridButtonNumbers(10, btnStringNumbers, btnNumbers);
-            createGridButtonOthersOperation(5, btnStringOthers, btnOthers);
-            createGridButtonOperation(5, btnStringOperators, btnOperators);
-            
-            //Creating a text field
-            BorderPane pane = createBorderPaneCalculadora();
-            createGridPaneCalculadora(btnNumbers, btnOperators, btnOthers, gridPane);
+            createGridButtonOthersOperation(4, btnStringOthers, btnOthers);
+            createGridButtonOperation(4, btnStringOperators, btnOperators);
+            createGridButtonMedia(5, btnStringMedia, btnMedia);
 
+            //Creating a text field
+            BorderPane pane = createBorderPaneCalculadora(btnMedia);
+            createGridPaneCalculadora(btnNumbers, btnOperators, btnOthers, gridPane);
             pane.setCenter(gridPane);
-            
 
             Scene scene = new Scene(pane, 300, 275);
             stage.setTitle("Calculadora");
@@ -47,15 +48,12 @@ public class CalculadoraView extends Application {
         }
 
 
-        private static BorderPane createBorderPaneCalculadora() {
+        private static BorderPane createBorderPaneCalculadora(Button[] btnMedia) {
             BorderPane borderPane = new BorderPane();
             BorderPane borderPaneTopBottom = new BorderPane();
-
             borderPane.setPadding(new Insets(20, 0, 20, 20));
-            
             HBox hbox = new HBox();
-            hbox.getChildren().addAll();
-
+            hbox.getChildren().addAll(btnMedia[0], btnMedia[1], btnMedia[2], btnMedia[3], btnMedia[4]);
             borderPaneTopBottom.setRight(CalculadoraController.textField);
             borderPaneTopBottom.setBottom(hbox);
             borderPane.setTop(borderPaneTopBottom);
@@ -69,38 +67,59 @@ public class CalculadoraView extends Application {
 
             gridPane.add(btnOthers[0], 0, 0);
             gridPane.add(btnOthers[1], 1, 0);
-            
-            for(i = 0; i < 5; i++) {
-                gridPane.add(btnOperators[i], 3, i + 1   );
+            gridPane.add(btnOthers[2], 2, 0);
+
+            for(i = 0; i < 4; i++) {
+                gridPane.add(btnOperators[i], 3, i);
             }
-            
-            for(i = 0, count = 7; i < 10; i++, count = count - 3) {
+
+            for(i = 2, count = 7; i < 5; i++, count = count - 3) {
                 for(j = 0; j < 3; j++){
-                    gridPane.add(btnNumbers[i], 4, i + 1);
+                    gridPane.add(btnNumbers[count + j], j, i);
                 }
             }
             
-            gridPane.add(btnOthers[2], 0, 5);
-            gridPane.add(btnOthers[3], 2, 5);
+            gridPane.add(btnOthers[3], 0, 5);
 
             return gridPane;
+        }
+
+        private static Button[] createGridButtonMedia(int size, String[] btnStringMedia, Button[] btnMedia) {
+            for(int i = 0; i < size; i++) {
+                btnMedia[i] = new Button(btnStringMedia[i]);
+                btnMedia[i].setStyle(STYLESHEET_CASPIAN);
+                String j = btnStringMedia[i];
+                btnMedia[i].setOnAction(
+                    (ActionEvent event) -> CalculadoraController.computar(j)
+                );
+            }
+            return btnMedia;
         }
         
         private static Button[] createGridButtonNumbers(int length, String[] btnString, Button[] btn){
             for (int i = 0; i < length; i++) {
                 btn[i] = new Button(btnString[i]);
                 btn[i].setPrefSize(50, 50);
-                btn[i].setStyle("-fx-background-color: #A9A9A9; -fx-text-fill: #FFFFFF;");
+                btn[i].setStyle(STYLESHEET_CASPIAN);
+                String j = btnString[i];
+                btn[i].setOnAction(
+                    (ActionEvent event) -> CalculadoraController.computar(j)
+                );
             }
 
             return btn;
         }
 
+
         private static Button[] createGridButtonOperation(int length, String[] btnString, Button[] btn){
             for (int i = 0; i < length; i++) {
                 btn[i] = new Button(btnString[i]);
                 btn[i].setPrefSize(50, 50);
-                btn[i].setStyle("-fx-background-color: #A9A9A9; -fx-text-fill: #FFFFFF;");
+                btn[i].setStyle("-fx-base:white");
+                String j = btnString[i];
+                btn[i].setOnAction(
+                    (ActionEvent event) ->CalculadoraController.computar(j)
+                );
             }
             return btn;
         }
@@ -112,7 +131,7 @@ public class CalculadoraView extends Application {
                 btnOthers[i].setPrefSize(50, 50);
                 String j = btnStringOthers[i];
                 btnOthers[i].setOnAction(
-                    (ActionEvent e) -> CalculadoraCont0roller.computar(j)
+                    (ActionEvent e) -> CalculadoraController.computar(j)
                 );
             }
             return btnOthers;
